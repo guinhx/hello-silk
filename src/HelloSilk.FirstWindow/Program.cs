0ce4f4f0de07c1b9e67ffada8301e5e3efe6ed1e
Diego Santos de Araujo <diego.araujo@digicade.com.br>
2026-01-07 09:25:28 -0300

    first window using Silk.Net

--- src/HelloSilk.FirstWindow/Program.cs
+++ src/HelloSilk.FirstWindow/Program.cs
@@ -1,2 +1,11 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using Silk.NET.Input;
+using Silk.NET.Maths;
+using Silk.NET.Windowing;
+
+var options = WindowOptions.Default with
+{
+    Size = new Vector2D<int>(800, 600),
+    Title = "My first Silk.NET application!"
+};
+var window = Window.Create(options);
+window.Run();
