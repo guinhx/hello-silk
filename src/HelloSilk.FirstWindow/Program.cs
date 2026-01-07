128209957e10048a5fe8f1c95e4200a753c030d9
Diego Teles <diegosantosaraujo@outlook.com>
2026-01-07 09:40:32 -0300

    First window events and input handling

--- src/HelloSilk.FirstWindow/Program.cs
+++ src/HelloSilk.FirstWindow/Program.cs
@@ -8,4 +8,30 @@ var options = WindowOptions.Default with
     Title = "My first Silk.NET application!"
 };
 var window = Window.Create(options);
+
+window.Load += () =>
+{
+    Console.WriteLine("Window loaded!");
+
+    var input = window.CreateInput();
+    for (var i = 0; i < input.Keyboards.Count; i++) {
+        var keyboard = input.Keyboards[i];
+        keyboard.KeyDown += (IKeyboard keyboard, Key key, int keyCode) =>
+        {
+            Console.WriteLine($"Key {key} pressed!");
+        };
+    }
+};
+
+window.Update += (double deltaTime) =>
+{
+    // change window title
+    window.Title = $"My first Silk.NET application! Dt. {deltaTime:F2}";
+};
+
+window.Render += (double deltaTime) =>
+{
+    // Rendering goes here
+};
+
 window.Run();
